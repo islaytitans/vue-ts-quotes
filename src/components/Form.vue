9951efe64df6145502ac7778a3ab966d0a6c6c01
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2020-05-14 09:33:39 +0100

    submit the form with prevent default

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -128,12 +128,14 @@
         <div
           class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitForm">
+            Submit!
+          </button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -174,8 +176,14 @@ export default Vue.extend({
       sendMail: new Array<unknown>(),
       priorities: ["High", "Medium", "Low"],
       selectedPriority: "High",
-      dataSwitch: true
+      dataSwitch: true,
+      isSubmitted: false,
     };
+  },
+  methods: {
+    submitForm(): void {
+      this.isSubmitted = true;
+    },
   },
   components: {
     appSwitch: Switch,
