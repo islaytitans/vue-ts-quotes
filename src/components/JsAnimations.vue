3192da742eef3bd0f83c4f42a3587e4656e69ad1
jonathanmrobbins <jonathanmrobbins@penguin.lxd>
2020-06-14 12:23:11 +0100

    removed unrequired css styles for the js animation

--- src/components/JsAnimations.vue
+++ src/components/JsAnimations.vue
@@ -20,6 +20,8 @@
         >
           <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
         </transition>
+        <hr>
+
       </div>
     </div>
   </div>
@@ -32,7 +34,6 @@ export default Vue.extend({
     return {
       show: false,
       load: true,
-      alertAnimation: "fade",
       elementWidth: 100
     };
   },
@@ -88,59 +89,5 @@ export default Vue.extend({
 </script>
 
 <style>
-.fade-enter {
-  opacity: 0;
-}
 
-.fade-enter-active {
-  transition: opacity 1s;
-}
-
-.fade-leave {
-  /* Not required but adding for info
-  opacity: 1; */
-}
-
-.fade-leave-active {
-  transition: opacity 1s;
-  opacity: 0;
-}
-
-.slide-enter {
-  opacity: 0;
-  /* Not required but adding for info
-    transform: translateY(20px); */
-}
-
-.slide-enter-active {
-  animation: slide-in 1s ease-out forwards;
-  transition: opacity 0.5s;
-}
-
-.slide-leave {
-}
-
-.slide-leave-active {
-  animation: slide-out 1s ease-out forwards;
-  transition: opacity 1s;
-  opacity: 0;
-}
-
-@keyframes slide-in {
-  from {
-    transform: translateY(20px);
-  }
-  to {
-    transform: translateY(0);
-  }
-}
-
-@keyframes slide-out {
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(20px);
-  }
-}
 </style>
